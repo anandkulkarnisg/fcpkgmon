4eaa3c3bc3dc272769aba350dc737050a11d9d83
Anand Kulkarni <anand@localhost.localdomain>
2020-01-02 21:13:04 +0800

    Adding initial tested cut on games.Most basic design is done. Needs improvement and dynamic configuration features.

---
+++ custom/steam.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+isSteamInstalled=$(dnf repolist|grep -i "negativo17 - Steam"|wc -l|sed "s/ //g")
+if [ $isSteamInstalled -eq 0 ]; then
+	dnf config-manager --add-repo=http://negativo17.org/repos/fedora-steam.repo
+	dnf install -y steam
+fi
