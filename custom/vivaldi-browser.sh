734505b449126f75066e513b6f9b968ff52302aa
root <root@ldnpsr2937.intranet.barcapint.com>
2020-08-16 14:05:39 +0800

    Anand Kulkarni - 16/08/2020 14:05:38:758 Asia/Singapore(+08,+0800) - commited via autocommit tool.

---
+++ custom/vivaldi-browser.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+# Check if package installation is needed first.
+isVivaldiBrowserInstalled=$(dnf repolist|grep -i "vivaldi"|wc -l|sed "s/ //g")
+isVivaldiStablePkgInstalled=$(rpm -qa|grep -i "vivaldi-stable"|wc -l|sed "s/ //g")
+
+# If only then install the repo definition and package.
+if [ $isVivaldiBrowserInstalled -eq 0 ]; then
+	  dnf config-manager --add-repo https://repo.vivaldi.com/archive/vivaldi-fedora.repo
+else
+  echo -e "vivaldi dnf repo already configured!"
+fi
+
+if [ $isVivaldiStablePkgInstalled -eq 0 ]; then
+	dnf install -y vivaldi-stable
+else
+  echo -e "vivaldi-stable already installed."
+fi
+
+exit 0
