15eb0840adc7ca8c1a09e9dcbffe6d7b4de32711
anandkulkarnisg <anand.kulkarni.sg@gmail.com>
2020-09-06 18:26:46 +0800

    Anand Kulkarni - 06/09/2020 18:26:46:829 Asia/Singapore(+08,+0800) - commited via autocommit tool.

--- custom/sublime-text-editor.sh
+++ custom/sublime-text-editor.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/bash
 # SubLime Editor for Linux.
 # https://www.sublimetext.com/docs/3/linux_repositories.html#dnf
-rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
-dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-dnf install -y sublime-text
+
+isRepoInstalled=$(dnf repolist|grep "sublime-text"|wc -l)
+
+if [ $isRepoInstalled -eq 0 ]; then
+	rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+	dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+else
+	echo -e "Seems the sublime editor repository is already installed.."
+fi
+
+isEditorInstalled=$(rpm -qa|grep "sublime-text"|wc -l)
+if [ $isEditorInstalled -eq 0 ]; then
+	dnf install -y sublime-text
+else
+	echo -e "sublime editor is already installed.."
+fi
